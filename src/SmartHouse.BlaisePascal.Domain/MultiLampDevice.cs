2ebeb4c1bf5f68b09a74653cb801131706360f23
bryan.casali <bryan.casali.stud@ispascalcomandini.it>
2025-11-11 21:00:04 +0100

    Added MultiLampDevice class

---
+++ src/SmartHouse.BlaisePascal.Domain/MultiLampDevice.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.BlaisePascal.Domain
+{
+    public class MultiLampDevice
+    {
+        public List<AbstractLamp> Lamp;
+
+        public int Quantity { get; set; }
+        public MultiLampDevice(int quantity, AbstractLamp lamp)
+        {
+            Quantity = quantity;
+
+            Lamp = new List<AbstractLamp>();
+
+            for (int i = 0; i < Quantity; i++)
+            {
+                Lamp.Add(lamp);
+            }
+        }
+
+        public void TurnOff()
+        {
+            for (int i = 0; i < Quantity; i++)
+            {
+                Lamp[i].TurnOff();
+            }
+        }
+
+
+        public void TurnOn()
+        {
+            for (int i = 0; i < Quantity; i++)
+            {
+                Lamp[i].TurnOn();
+            }
+        }
+        public void ChangeBrightness(bool lamp, int brightness, int position)
+        {
+            for (int i = 0; i < Quantity; i++)
+            {
+                Lamp[i].ChangeBrightness(brightness);
+            }
+        }
+    }
+}
+
