22aea2bb78cc9ba6ad020b3b36c6f8282f183085
T0mmyChiucchi <tommaso.chiucchi.stud@ispascalcomandini.it>
2026-02-20 09:52:23 +0100

    .

--- src/SmartHouse.BlaisePascal.Application/ElectroDomestics/AirConditioner/Commands/AddAirConditionerCommand.cs
+++ src/SmartHouse.BlaisePascal.Application/ElectroDomestics/AirConditioner/Commands/AddAirConditionerCommand.cs
@@ -1,9 +1,20 @@
-﻿using SmartHouse.BlaisePascal.Domain.ElectroDomestics.AirConditioner.Repository;
+﻿using SmartHouse.BlaisePascal.Domain.ElectroDomestics.AirConditioner;
+using SmartHouse.BlaisePascal.Domain.ElectroDomestics.AirConditioner.Repository;
 
 namespace SmartHouse.BlaisePascal.Application.ElectroDomestics.AirConditioner.Commands
 {
     public class AddAirConditionerCommand
     {
-        private IAirConditionerRepository _airconditionerrepository;
+        private IAirConditionerRepository _repository;
+
+        public AddAirConditionerCommand(IAirConditionerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Execute(string name)
+        {
+
+        }
     }
 }
