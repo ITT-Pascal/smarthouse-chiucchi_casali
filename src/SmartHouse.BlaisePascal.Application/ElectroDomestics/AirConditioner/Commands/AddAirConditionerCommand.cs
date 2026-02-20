59da3eb08f83855fa566dfe2ec8a3ec87ce5a119
T0mmyChiucchi <tommaso.chiucchi.stud@ispascalcomandini.it>
2026-02-20 08:12:53 +0100

    Added folders

---
+++ src/SmartHouse.BlaisePascal.Application/ElectroDomestics/AirConditioner/Commands/AddAirConditionerCommand.cs
@@ -0,0 +1,9 @@
+﻿using SmartHouse.BlaisePascal.Domain.ElectroDomestics.AirConditioner.Repository;
+
+namespace SmartHouse.BlaisePascal.Application.ElectroDomestics.AirConditioner.Commands
+{
+    public class AddAirConditionerCommand
+    {
+        private IAirConditionerRepository _airconditionerrepository;
+    }
+}
