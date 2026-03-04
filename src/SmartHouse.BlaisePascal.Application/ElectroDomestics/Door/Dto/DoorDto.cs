bb377b5c97b7cbe0b9ae11f9c9be0460f4e2e2db
T0mmyChiucchi <tommaso.chiucchi.stud@ispascalcomandini.it>
2026-03-04 11:00:35 +0100

    Added MY Dtos

---
+++ src/SmartHouse.BlaisePascal.Application/ElectroDomestics/Door/Dto/DoorDto.cs
@@ -0,0 +1,27 @@
+﻿namespace SmartHouse.BlaisePascal.Application.ElectroDomestics.Door.Dto
+{
+    public class DoorDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public int Pin { get; set; }
+        public string DoorStatus { get; set; }
+        public string LockStatus { get; set; }
+        public DateTime CreationTime_UTC { get; set; }
+        public DateTime LastModification_UTC { get; set; }
+
+        public override string ToString()
+        {
+            return
+                $"Id: {Id}.\n" +
+                $"Name: {Name}.\n" +
+                $"Status: {Status}.\n" +
+                $"Pin: {Pin}.\n" +
+                $"DoorStatus: {DoorStatus}.\n" +
+                $"LockStatus: {LockStatus}.\n" +
+                $"CreationTime: {CreationTime_UTC}.\n" +
+                $"LastModification: {LastModification_UTC}.";
+        }
+    }
+}
