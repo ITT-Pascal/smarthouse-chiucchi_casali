fcea6277daf5c24fda1d8d082eb0a5be63138945
T0mmyChiucchi <tommaso.chiucchi.stud@ispascalcomandini.it>
2026-03-05 12:13:59 +0100

    Added AirConditione mapper

---
+++ src/SmartHouse.BlaisePascal.Application/ElectroDomestics/AirConditioner/Mapper/AirConditionerMapper.cs
@@ -0,0 +1,19 @@
+﻿using SmartHouse.BlaisePascal.Application.ElectroDomestics.AirConditioner.Dto;
+
+namespace SmartHouse.BlaisePascal.Application.ElectroDomestics.AirConditioner.Mapper
+{
+    public class AirConditionerMapper
+    {
+        public static AirConditionerDto ToDto(AirConditioner airConditioner)
+        {
+            return new AirConditionerDto
+            {
+                Id= airConditioner.Id,
+                Name = airConditioner.Name,
+                Status = airConditioner.Status,
+                CreationTime_UTC = airConditioner.CreationTime_UTC,
+                LastModification_UTC = airConditioner.LastModification_UTC,
+            }
+        }
+    }
+}
