f9833a67f7073f483c72611b65c323c56f497ade
bryan.casali <bryan.casali.stud@ispascalcomandini.it>
2026-03-06 08:51:39 +0100

    Fixed .Net issues

--- src/SmartHouse.BlaisePascal.Console/Program.cs
+++ src/SmartHouse.BlaisePascal.Console/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+class program
+{
+    static void Main()
+    {
+        ILampRepository repository = new InMemoryLampRepository();
+        ILampRepository repositorytxt = new TxtLampRepository();
+        LampController controller = new LampController(repository);
+
+        bool exit = false;
+
+        while (!exit)
+        {
+            Clear();
+            controller.ShowLamps();
+            ShowMenu();
+
+            Write("Choose an option: ");
+            string choice = ReadLine();
+
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case "1"
+                    controller.Add
+            }
+        }
+    }
+}
